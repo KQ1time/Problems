108d80d5e8498937b5099c68fb483c0bf25061ed
Alexander <kq1time.me@mail.ru>
2026-07-02 18:13:29 +0300

    Create grid.cpp

---
+++ grid/grid.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+    int x1, y1, x2, y2;
+    std::cin >> x1;
+    std::cin >> y1;
+    std::cin >> x2;
+    std::cin >> y2;
+
+    int result1, result2;
+    result1 = x1 - x2;
+    result2 = y1 - y2;
+
+    if (result1 < 0) {
+        for (int i = 0; i < std::abs(result1); ++i) {
+            std::cout << "E";
+        }
+    } else if (result1 > 0) {
+        for (int i = 0; i < std::abs(result1); ++i) {
+            std::cout << "W";
+        }
+    }
+
+    if (result2 < 0) {
+        for (int i = 0; i < std::abs(result2); ++i) {
+            std::cout << "N";
+        }
+    } else if (result2 > 0) {
+        for (int i = 0; i < std::abs(result2); ++i) {
+            std::cout << "S";
+        }
+    }
+    return 0;
+}
